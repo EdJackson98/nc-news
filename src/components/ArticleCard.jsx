cd94e36a47a82948ea8c518f2636b66841ccc403
Ed <erj@sky.com>
2023-09-05 11:58:21 +0100

    added image to article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,6 +1,7 @@
 const ArticleCard = ({ article }) => {
     return (
         <li className="ArticleCard">
+            <img src={article.article_img_url} width="100px"></img>
             <h3>{article.title}</h3>
             <h3>{article.author}</h3>
             <p>Votes: {article.votes}</p>
