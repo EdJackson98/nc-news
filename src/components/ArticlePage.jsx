1129eab5861c624aa0d721ff864fac73f0e0f676
Ed <erj@sky.com>
2023-09-07 11:58:28 +0100

    voting button now works, clickable loads of times though so probably need to introduce login context next

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ArticleHeader from './ArticleHeader';
-import { getArticlePage } from '../utils/api';
+import { getArticlePage, patchArticleVote } from '../utils/api';
 import CommentsList from "./CommentsList.jsx"
 
 const ArticlePage = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState(null);
+  const [voteCount, setVoteCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getArticlePage(article_id)
       .then((data) => {
         setArticle(data.article);
+        setVoteCount(data.article.votes);
       });
   }, [article_id]);
+
+  const handleUpvote = () => {
+    if (isLoading) {
+      return;
+    }
+    setVoteCount(voteCount + 1);
+    setIsLoading(true);
+    patchArticleVote(article_id, 1)
+      .then(() => {
+        setIsLoading(false);
+      })
+  };
 
   if (!article) {
     return <div>Loading...</div>;
@@ -27,9 +42,9 @@ const ArticlePage = () => {
     <div className="ArticlePage">
         <img className="APImage" src={article.article_img_url} width="100px" alt={article.title} />
         <h3 className="APTitle">{article.title}</h3>
-        <p className="APVotes">Votes: {article.votes}</p>
+        <p className="APVotes">Votes: {voteCount}</p>
         <p className="APComments">Comments: {article.comment_count}</p>
-        <button className="UpvoteButton">Upvote!</button>
+        <button className="UpvoteButton" onClick={handleUpvote}>Upvote!</button>
         <p className="ArticleBody">{article.body}</p>
       </div>
     <CommentsList article_id={article_id}/>
